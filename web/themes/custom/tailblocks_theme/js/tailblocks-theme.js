9b34af0b22272b5f622d36f67cfa799aed8aa4eb
Van Duong <van.duong@stickyeyes.com>
2024-06-17 14:42:35 +0100

    feat: created custom theme

---
+++ web/themes/custom/tailblocks_theme/js/tailblocks-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * tailblocks_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.tailblocksTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
